2e186e296608bdae62c90185608d62d1938b6ce6
Minnela Muralikrishna <147706250+MinnelaMK@users.noreply.github.com>
2025-05-23 08:06:05 +0530

    Add files via upload

---
+++ TestNG/Testngdemo.java
@@ -0,0 +1,14 @@
+import com.google.common.annotations.VisibleForTesting;
+
+public class Testngdemo 
+{
+	@Test
+	
+	public void testcase1()
+	{
+		System.out.println("In Test Case 1");
+	// TODO Auto-generated method stub
+
+	}
+
+}
